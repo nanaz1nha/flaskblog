272318169fbff249018eb643a35663454ed6df42
André Luferat <33375030+Luferat@users.noreply.github.com>
2024-09-13 19:34:49 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,17 +1,22 @@
-from flask import Flask
+# Importa a classe Flask do módulo flask
+from flask import Flask  
 
-app = Flask(__name__)
+# Cria uma instância da aplicação Flask
+app = Flask(__name__)  
 
-
+# Define a rota para a URL raiz ('/')
 @app.route('/')
 def home():
-    return "Hello World!"
+    # Retorna uma mensagem simples
+    return "Hello World!"  
 
-
+# Define a rota para a URL '/contatos'
 @app.route('/contatos')
 def peteca():
-    return "Diga o que você quer agora!"
+    # Retorna uma mensagem simples
+    return "Diga o que você quer agora!"  
 
-
+# Verifica se o script está sendo executado diretamente
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Inicia o servidor Flask em modo debug
+    app.run(debug=True)  
