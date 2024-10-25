24bdd0b929fb52e1158d37557bf9b2c94d7a835c
nanaz1nha <thaina_olliveira@hotmail.com>
2024-10-25 11:16:02 -0300

    novas atualizações

---
+++ config_production.py
@@ -0,0 +1,5 @@
+# Configurações do MySQL para produção (PythonAnyWhere)
+MYSQL_HOST = 'joquinha.mysql.pythonanywhere-services.com'
+MYSQL_USER = 'joquinha'
+MYSQL_PASSWORD = 'M3uMySQL_py'
+MYSQL_DB = 'joquinha$default'
